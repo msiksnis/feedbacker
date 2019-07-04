14eb85b02acb7c4bc1ec23340af3d76839a64f0f
msiksnis <msiksnis@gmail.com>
2019-07-04 21:20:39 +0200

    added users module, connected mongodb, added logic in passport.js file to check if the user already exist if not then creates a new user

--- services/passport.js
+++ services/passport.js
@@ -1,6 +1,9 @@
 const passport = require("passport");
+const mongoose = require("mongoose");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
 const keys = require("../config/keys");
+
+const User = mongoose.model("users");
 
 passport.use(
   new GoogleStrategy(
@@ -10,9 +13,18 @@ passport.use(
       callbackURL: "/auth/google/callback"
     },
     (accessToken, refreshToken, profile, done) => {
-      console.log("access token", accessToken);
-      console.log("refresh token", refreshToken);
-      console.log("profile:", profile);
+      // Checks if user with that googleId already exist in db
+      User.findOne({ googleId: profile.id }).then(existingUser => {
+        if (existingUser) {
+          // When we already have a cord with the given profile Id
+          done(null, existingUser);
+        } else {
+          // When we don't have a record with this Id. Make a new recortd!
+          new User({ googleId: profile.id })
+            .save()
+            .then(user => done(null, user));
+        }
+      });
     }
   )
 );
